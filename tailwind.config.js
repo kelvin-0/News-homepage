1bc2e400c12cec4695008644c9697103bba62443
kelvin-0 <113959819+kelvin-0@users.noreply.github.com>
2022-11-17 17:49:16 +0700

    Responsive Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    colors: {
-      "soft-orange": "hsl(35, 77%, 62%)",
-      "soft-red": "hsl(5, 85%, 63%)",
-      "off-white": "hsl(36, 100%, 99%)",
-      "grayish-blue": "hsl(233, 8%, 79%)",
-      "dark-grayish-blue": "hsl(236, 13%, 42%)",
-      "very-dark-blue": "hsl(240, 100%, 5%)"
-    },
     extend: {
       fontFamily: {
         "Inter": ["Inter", "sans-serif"]
+      },
+      colors: {
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": "hsl(233, 8%, 79%)",
+        "dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "very-dark-blue": "hsl(240, 100%, 5%)"
       },
     },
   },
