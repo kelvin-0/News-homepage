068946528b09d084ee082c419f5f565d3c3815a5
kelvin-0 <113959819+kelvin-0@users.noreply.github.com>
2022-11-17 22:53:12 +0700

    Desktop finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
         "dark-grayish-blue": "hsl(236, 13%, 42%)",
         "very-dark-blue": "hsl(240, 100%, 5%)"
       },
+      backgroundImage:{
+        'mobile': "url('/assets/images/image-web-3-mobile.jpg')",
+        'desktop': "url('/assets/images/image-web-3-desktop.jpg')",
+      }
     },
   },
   plugins: [],
